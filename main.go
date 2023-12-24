260aa414739d623e3f49b2d22a108505c6a5371f
Fiifi Amoah <fyfybaisieamoah6@gmail.com>
2023-12-24 19:15:22 +0000

    Task crud updated

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
@@ -17,6 +18,42 @@ type User struct {
 	ID       uint   `gorm:"primary_key"`
 	Username string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
+	Tasks    []Task `gorm:"foreignKey:UserID" json:"tasks"`
+}
+
+type Task struct {
+	ID          uint      `gorm:"primary_key"`
+	Title       string    `gorm:"not null" json:"title"`
+	Description string    `gorm:"not null" json:"description"`
+	DueDate     time.Time `gorm:"not null" json:"due_date"`
+	Completed   bool      `gorm:"not null" json:"completed"`
+	UserID      uint      `gorm:"not null" json:"user_id"`
+}
+
+func createTask(c *gin.Context) {
+	var userInput struct {
+		Title       string    `json:"title" binding:"required"`
+		Description string    `json:"description" binding:"required"`
+		DueDate     time.Time `json:"due_date" binding:"required"`
+		UserID      uint      `json:"user_id" binding:"required"`
+	}
+
+	if err := c.BindJSON(&userInput); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	task := Task{
+		Title:       userInput.Title,
+		Description: userInput.Description,
+		DueDate:     userInput.DueDate,
+		Completed:   false,
+		UserID:      userInput.UserID,
+	}
+
+	db.Create(&task)
+
+	c.JSON(200, gin.H{"message": "Task created successfully"})
 }
 
 func hashPassword(password string) (string, error) {
@@ -47,7 +84,6 @@ func registerUser(c *gin.Context) {
 	}
 
 	c.JSON(200, gin.H{"username": "User Successfully Registered"})
-
 }
 
 func loginUser(c *gin.Context) {
@@ -78,6 +114,37 @@ func loginUser(c *gin.Context) {
 
 }
 
+func markTaskAsCompleted(c *gin.Context) {
+	taskID := c.Param("id")
+
+	var task Task
+
+	if err := db.First(&task, taskID).Error; err != nil {
+		c.JSON(400, gin.H{"error": "Task not found!"})
+		return
+	}
+
+	task.Completed = true
+	db.Save(&task)
+
+	c.JSON(200, gin.H{"message": "Task marked as completed!"})
+}
+
+func deleteTask(c *gin.Context) {
+	taskID := c.Param("id")
+
+	var task Task
+
+	if err := db.First(&task, taskID).Error; err != nil {
+		c.JSON(400, gin.H{"error": "Task not found!"})
+		return
+	}
+
+	db.Delete(&task)
+
+	c.JSON(200, gin.H{"message": "Task deleted successfully!"})
+}
+
 func main() {
 	dns := "user=postgres password='codeck prime' dbname=todolist port=5432 sslmode=disable"
 
@@ -93,6 +160,10 @@ func main() {
 
 	router.POST("/register", registerUser)
 	router.POST("/login", loginUser)
+
+	router.POST("/tasks", createTask)
+	router.PUT("/tasks/:id", markTaskAsCompleted)
+	router.DELETE("/tasks/:id", deleteTask)
 
 	router.Run(":5000")
 	fmt.Println("Server is running on port 5000")
