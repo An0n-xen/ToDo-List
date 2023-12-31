4e336499cd4645971aafcf51db83e88bdf25824f
Fiifi Amoah <fyfybaisieamoah6@gmail.com>
2023-12-31 12:10:48 +0000

    design of add task done

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,12 +1,36 @@
 "use client";
 
-import React, { useEffect } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import { Calendar } from "@/components/ui/calendar";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { useAuthContext } from "../Context/user";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { UpdateIcon } from "@radix-ui/react-icons";
 
 const MainPage = () => {
-  const { isLoggedIn, setIsLoggedIn } = useAuthContext();
+  const { isLoggedIn, user } = useAuthContext();
+  const [date, setDate] = useState<Date>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -16,9 +40,71 @@ const MainPage = () => {
   }, [isLoggedIn, router]);
 
   return (
-    <div>
+    <div className="w-screen h-screen flex flex-col">
       <Navbar />
-      <div className="">page</div>
+      <div className="px-16 mt-10 flex flex-col h-full mb-10">
+        <div className="lg:mt-5 h-fit">
+          <p className="text-2xl font-mono lg:text-3xl">
+            🖐 Welcome {user.toLocaleUpperCase()} 😎
+          </p>
+        </div>
+
+        <div className="flex-grow flex-shrink mt-10 lg:mt-16">
+          <div className="border-r-2 w-44 h-full lg:w-60 text-center pt-10 lg:pt-5">
+            <Dialog>
+              <DialogTrigger>
+                <Button>Add Task</Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Add Task</DialogTitle>
+                  <DialogDescription>
+                    Add new task to your list
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="flex flex-col gap-3">
+                  <Input
+                    placeholder="Task Name"
+                    className="w-full mt-2"
+                    type="text"
+                  />
+                  <Input
+                    placeholder="Task Description"
+                    className="w-full"
+                    type="text"
+                  />
+                </div>
+                <Popover>
+                  <PopoverTrigger>
+                    <Button className="mt-4 w-full">
+                      <CalendarIcon className="w-5 h-5 mr-2" />
+                      {date ? date.toLocaleDateString() : "Select Date"}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <Calendar
+                      mode="single"
+                      selected={date}
+                      onSelect={setDate}
+                    />
+                  </PopoverContent>
+                </Popover>
+                <DialogClose>
+                  <div className="w-full flex">
+                    <Button className="w-40 ml-auto">
+                      {!isLoading && (
+                        <UpdateIcon className="w-5 h-5 mr-2 animate-spin" />
+                      )}
+                      Done
+                    </Button>
+                  </div>
+                </DialogClose>
+              </DialogContent>
+            </Dialog>
+          </div>
+          <div className="flex-grow"></div>
+        </div>
+      </div>
     </div>
   );
 };
